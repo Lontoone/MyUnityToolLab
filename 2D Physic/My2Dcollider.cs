7c2cc3db76e4ea12f2d228591355e7f0231827f8
Lontoone <45337164+Lontoone@users.noreply.github.com>
2020-07-08 17:08:10 +0800

    Add files via upload
    Now you can get a list of objs that is colliding with.
    可以取的目前正在collide的物件清單

--- 2D Physic/My2Dcollider.cs
+++ 2D Physic/My2Dcollider.cs
@@ -5,20 +5,12 @@ using System;
 [RequireComponent(typeof(Collider2D))]
 public class My2Dcollider : MonoBehaviour
 {
-    //[HideInInspector]
-    //public Vector2 size = new Vector2(1, 1);
-    //[HideInInspector]
-    //public Vector2 offset = new Vector2(0, 0);
-
     public LayerMask entity_layer;
 
     public event Action<GameObject> OnColliderEnter;
     public event Action<GameObject> OnColliderStay;
     public event Action<GameObject> OnColliderLeave;
-    bool isStaying = false;
-    GameObject last_Collide_obj;
 
-    //bool can_hit = true;
     float hit_interval = 0.2f;
     Coroutine hit_check_coro;
 
@@ -31,60 +23,55 @@ public class My2Dcollider : MonoBehaviour
     {
         hit_interval = 0.05f;
     }
+    [SerializeField]
+    public List<Collider2D> current_colliders = new List<Collider2D>();//目前的collider
 
     private void Update()
     {
-        //RaycastHit2D hit_collider = Physics2D.BoxCast((Vector2)transform.position + offset, size, 0, Vector2.up, 0.5f, entity_layer);
         if (hit_check_coro == null)
         {
             hit_check_coro = StartCoroutine(Hit_Check_coro());
             Collider2D myCollider = gameObject.GetComponent<Collider2D>();
+
             Collider2D[] colliders = new Collider2D[10];
+
             ContactFilter2D contactFilter = new ContactFilter2D();
             contactFilter.SetLayerMask(entity_layer);
             int colliderCount = myCollider.OverlapCollider(contactFilter, colliders);
+
+            //檢查離開的 (紀錄有但這次沒有的)
+            for (int i = 0; i < current_colliders.Count; i++)
+            {
+                if (!Array.Exists(colliders, x => x == current_colliders[i]))
+                {
+                    Debug.Log("LEAVE " + current_colliders[i].gameObject.name);
+                    OnColliderLeave(current_colliders[i].gameObject);
+                    current_colliders.RemoveAt(i);
+                }
+            }
+
             for (int i = 0; i < colliderCount; i++)
             {
-                //Debug.Log(colliders[i].gameObject.name);
-
                 if (colliders[i].gameObject != gameObject)
                 {
-                    if (isStaying == false && OnColliderEnter != null)
+                    if (!current_colliders.Contains(colliders[i]) && OnColliderEnter != null)
                     {
                         OnColliderEnter(colliders[i].gameObject);
-                        isStaying = true;
+                        current_colliders.Add(colliders[i]);
                     }
+
                     else if (OnColliderStay != null)
                     {
                         OnColliderStay(colliders[i].gameObject);
-                        last_Collide_obj = colliders[i].gameObject;
-                    }
-
-
-                }
-                if (colliders[i] == last_Collide_obj)
-                {
-                    Debug.Log("LEAVE");
-                    isStaying = false;
-                    if (OnColliderLeave != null)
-                    {
-                        OnColliderLeave(last_Collide_obj);
                     }
                 }
-
             }
-            if (colliderCount <= 0 && isStaying)
-            {
-                isStaying = false;
-            }
-
         }
 
     }
 
     IEnumerator Hit_Check_coro()
     {
-
         yield return new WaitForSeconds(hit_interval);
         hit_check_coro = null;
     }
