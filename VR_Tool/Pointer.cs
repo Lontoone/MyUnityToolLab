d413dd4a500b5809a8facde1bd320f326cc47550
Lontoone <45337164+Lontoone@users.noreply.github.com>
2020-08-27 14:59:18 +0800

    Add files via upload

---
+++ VR_Tool/Pointer.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using System;
+
+public class Pointer : MonoBehaviour
+{
+    public float Length = 5;
+    public GameObject Dot;
+    public SteamVR_Input_Sources inputSource;
+    public SteamVR_Action_Boolean openInput;
+    bool isOpen = false;
+    private LineRenderer lineRenderer;
+
+    GameObject currentPointing_obj;
+
+    public static event Action<GameObject, Vector3> ePointer_hover;
+    public static event Action<GameObject> ePointer_leave;
+    public static event Action<GameObject> ePointer_down;
+    private void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+
+        lineRenderer.enabled = isOpen;
+        Dot.gameObject.SetActive(isOpen);
+    }
+
+    private void Update()
+    {
+        if (openInput.GetStateDown(inputSource))
+        {
+            //按下按鈕
+            if (isOpen)
+            {
+                RaycastHit down_hit = CreateRaycast(Length);
+                if (down_hit.collider != null && ePointer_down != null) { ePointer_down(down_hit.collider.gameObject); }
+            }
+            isOpen = !isOpen;
+
+            lineRenderer.enabled = isOpen;
+            Dot.gameObject.SetActive(isOpen);
+        }
+        if (!isOpen) { return; }
+
+        //updateLine
+        float targetLength = Length;
+        RaycastHit hit = CreateRaycast(targetLength);
+        Vector3 endPosition = transform.position + (transform.forward * targetLength);
+
+        //check Leave
+        if ((hit.collider == null || currentPointing_obj != hit.collider.gameObject) && ePointer_leave != null)
+        {
+            ePointer_leave(currentPointing_obj);
+        }
+
+        if (hit.collider != null)
+        {
+            endPosition = hit.point;
+            if (ePointer_hover != null)
+                //hover
+                ePointer_hover(hit.collider.gameObject, hit.point);
+        }
+        currentPointing_obj = hit.collider == null ? null : hit.collider.gameObject;
+
+        Dot.transform.position = endPosition;
+
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, endPosition);
+
+    }
+    private RaycastHit CreateRaycast(float length)
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, transform.forward);
+        Physics.Raycast(ray, out hit, length);
+        return hit;
+    }
+}
---
+++ VR_Tool/PointerReciver.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+//接收玩家UI點擊    
+public class PointerReciver : MonoBehaviour
+{
+    public UnityEvent pressedEvent;
+    public UnityEvent hoverEvent;
+    public UnityEvent leaveEvent;
+
+    Image btnImg;
+    Color originColor;
+    public Color hoverColor, pressColor;
+
+
+    private void Start()
+    {
+        Pointer.ePointer_down += OnPressDown;
+        Pointer.ePointer_leave += OnPointerLeave;
+        Pointer.ePointer_hover += OnHover;
+        btnImg = gameObject.GetComponent<Image>();
+      
+
+        if (gameObject.GetComponent<Image>() != null)
+        {
+            originColor = gameObject.GetComponent<Image>().color;
+        }
+    }
+    private void OnDestory()
+    {
+        Pointer.ePointer_down -= OnPressDown;
+        Pointer.ePointer_leave -= OnPointerLeave;
+        Pointer.ePointer_hover -= OnHover;
+    }
+    void OnHover(GameObject _obj, Vector3 cursorPos)
+    {
+        if (_obj == this.gameObject)
+        {
+            if (hoverEvent != null) hoverEvent.Invoke();//返回指標位置
+
+            //hover特效
+            if (btnImg != null)
+            {
+                btnImg.color = hoverColor;
+            }
+        }
+
+
+    }
+
+    void OnPressDown(GameObject _obj)
+    {
+        if (_obj == gameObject)
+        {
+            if (pressedEvent != null) pressedEvent.Invoke();
+
+            //效果
+            if (btnImg != null)
+            {
+                btnImg.color = pressColor;
+            }
+
+            Invoke("BacktoOrigineColor_afterClick", 0.25f);
+        }
+    }
+
+    void OnPointerLeave(GameObject _obj)
+    {
+        if (_obj == gameObject)
+        {
+            Debug.Log(_obj.name + "leave");
+            if (leaveEvent != null) leaveEvent.Invoke();
+
+            //btn特效
+            if (btnImg != null)
+                btnImg.color = originColor;
+        }
+    }
+
+    //按下去幾秒後再回去原本的顏色
+    void BacktoOrigineColor_afterClick()
+    {
+        if (btnImg != null)
+            btnImg.color = originColor;
+    }
+}
