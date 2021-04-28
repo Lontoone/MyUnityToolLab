b6477c52c0b2fbde4d32e3144f79b2bf3537da29
Lontoone <news2000tw@gmail.com>
2021-04-28 22:54:23 +0800

    Photon Fast StartUP

---
+++ Photon/RoomMatchFastStartUP/PlayerControl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+
+public class PlayerControl : MonoBehaviour
+{
+    PhotonView pv;
+    public float speed = 5;
+    PlayerManager playerManager;
+
+    public TextMeshProUGUI name_text;
+
+    public void Start()
+    {
+        pv = gameObject.GetComponent<PhotonView>();
+        //Manager產生player時，順便告訴player產生它的manager是誰
+        //用尋找該view ID的PlayerManager
+        playerManager = PhotonView.Find((int)pv.InstantiationData[0]).GetComponent<PlayerManager>();
+
+        if (!pv.IsMine)
+        {
+            Destroy(GetComponentInChildren<Camera>());
+        }
+
+        name_text.text = PhotonNetwork.NickName;
+    }
+
+
+
+    public void Update()
+    {
+        if (!pv.IsMine)
+            return;
+        transform.position = transform.position + new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * speed * Time.deltaTime;
+    }
+}
---
+++ Photon/RoomMatchFastStartUP/PlayerManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System.IO;
+public class PlayerManager : MonoBehaviour
+{
+
+
+    PhotonView pv;
+    GameObject controller;
+
+    public void Awake()
+    {
+        pv = GetComponent<PhotonView>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (pv.IsMine)
+        {
+            CreateController();
+        }
+    }
+
+    void CreateController()
+    {
+        Debug.Log("Instantiated Player Controller");
+        //Transform spawnPoint = SpawnManager.instance.GetSpawnpoint();
+        //controller = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerController"), spawnPoint.position, spawnPoint.rotation, 0, new object[] { pv.ViewID });
+        controller = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "Player"), Vector2.zero, Quaternion.identity, 0, new object[] { pv.ViewID });
+    }
+
+    public void Die()
+    {
+        PhotonNetwork.Destroy(controller);
+        CreateController();
+    }
+}
